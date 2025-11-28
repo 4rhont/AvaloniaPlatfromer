aeca82cbe7c2b16137d4be83254e956578cfd31f
emogotka <Kanova2004@yandex.ru>
2025-11-28 13:25:02 +0700

    Возвращаем потеряшку BoolToScaleXConverter.cs

---
+++ GameApp/Converts/BoolToScaleXConverter.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace GameApp.Converters
+{
+    public class BoolToScaleXConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b)
+                return b ? 1.0 : -1.0;
+            return 1.0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
